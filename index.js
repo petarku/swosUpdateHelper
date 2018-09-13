9f3844339c7675d526bc5a4ca75f99be69f4d0e6
Petar Vujovic <petar.vujovic@sap.com>
2018-09-13 16:38:21 +0100

    national teams scrapper

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const superliga = require('./src/leagues');
 const club = require('./src/club');
 const fs = require('fs');
 const leagues = require('./src/leagues.json');
+const nationalTeams = require('./src/nationalTeams.json');
 
 function getLeague (league) {
-	superliga
-		.getClubs(league.url)
+	superliga.getClubs(league.url)
 		.then(clubs => {
 			const playersPromises = clubs.map(club.getPlayers);
 			return Promise.all(playersPromises);
@@ -19,5 +19,22 @@ function getLeague (league) {
 		});
 }
 
+function getNationalTeams(nationalTeam) {
+	club.getNationalTeamPlayers(nationalTeam)
+		.then(nationalTeams => {
+			const playersPromises = nationalTeams.map(club.getNationalTeamPlayers);
+			return Promise.all(playersPromises);
+
+		})
+	.then(res => {
+		const str = JSON.stringify(res, null, 2);
+		fs.writeFileSync(`data/nationalTeam-${nationalTeam.name}.json`, str);
+		console.log(`File data/nationalTeam-${nationalTeam.name}.json created!`);
+	});	
+	
+
+}
+
 // leagues.forEach(getLeague);	// get all leaggues
 getLeague(leagues[0]);			// get 1 league - for tests
+//getNationalTeams(nationalTeams[0]) ; 
--- src/club.js
+++ src/club.js
@@ -36,6 +36,35 @@ async function parsePlayerRow (row) {
 	return { number, name, position, flags, value, timeInPlay, swosValue, imgUrl, age };
 }
 
+async function parseNationalPlayerRow (row) {
+	if (!row || !row.querySelector) return console.error('cannot find player row...');
+	const number = row.querySelector('.rn_nummer').innerHTML;
+	/*console.log(number) ; 
+	const playerSubtable = row.querySelectorAll('.posrela table tbody tr');
+	const playerLink = playerSubtable[0].querySelector('.hauptlink a');
+	const name = playerLink.innerHTML;
+	const url = BASE_URL + playerLink.href;
+	const timeInPlay = await parsePlayerStats(url);
+	const position = playerSubtable[1].querySelector('td').innerHTML;
+	const zenTriertArray = row.querySelectorAll('.zentriert');
+	const age = zenTriertArray[1].textContent ; 
+	const imgUrl = playerSubtable[0].querySelector('td:first-child img')
+		.getAttribute('data-src')
+		.replace('small', 'medium');
+
+	const flagImgs = row.querySelectorAll('td img.flaggenrahmen');
+	//const flags = Array.from(flagImgs).map(img => img.alt);
+
+	const cells = row.children;
+	const value = cells[cells.length - 1].childNodes[0].textContent;
+
+	//const valueStripped = convertStringValuetoNumber(value, 'Mill', 'Th.' , ' €');
+	const valueStripped = convertStringValuetoNumber(value, 'm', 'k' , '£');
+	const swosValue = getTheSwosValue(valueStripped);
+*/
+	return { number };
+}
+
 function getTheSwosValue (valueStripped) {
 	for (let i = 0; i < swosRange.length; i++) {
 		if (valueStripped >= swosRange[i].minValue && valueStripped < swosRange[i].maxValue) {
@@ -111,6 +140,28 @@ async function parseTable (res) {
 	return Promise.all(promises).then(players => ({ players, coach , formation }));
 }
 
+async function parseNationalTeamTable (res) {
+	const { document } = (new JSDOM(res)).window;
+	if (!document) return;
+	const rows = document.querySelectorAll('#yw1 table.items>tbody>tr');
+	const promises = Array.from(rows).map(parseNationalPlayerRow);
+	const coachName = document.querySelectorAll('.container-hauptinfo');
+	const coach = coachName[0].textContent.trim() ;
+	console.log(coach); 
+	var formation = 'unknown'; 
+
+	try {
+		const formationNode = document.querySelectorAll('.large-7.aufstellung-vereinsseite.columns.small-12.unterueberschrift.aufstellung-unterueberschrift');
+		 formation = formationNode[0].textContent.trim() ; 
+	  }
+	  catch(error) {
+		formation = 'unknown'; 
+	  }
+	
+
+	return Promise.all(promises).then(players => ({ players, coach , formation }));
+}
+
 
 function getPlayers (club) {
 	console.log(`Getting players' details for ${club.name}...`);
@@ -125,6 +176,23 @@ function getPlayers (club) {
 		});
 }
 
+function getNationalTeamPlayers (nationalTeam) {
+	
+	console.log(`Getting players' details for ${nationalTeam.name}...`);
+	
+	return fetch(BASE_URL + nationalTeam.url)
+		.then(res => res.text())
+		.then(parseNationalTeamTable)
+		.then(res => {
+			nationalTeam.coach = res.coach;
+			nationalTeam.formation = res.formation ; 
+			nationalTeam.players = res.players.sort((a, b) => b.timeInPlay - a.timeInPlay);
+			return nationalTeam;
+		});
+}
+
+
 module.exports = {
-	getPlayers
+	getPlayers, 
+	getNationalTeamPlayers 
 };
